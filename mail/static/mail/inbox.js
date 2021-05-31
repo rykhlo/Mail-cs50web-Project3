17e5e456832a743b7c1abdf4ac61791140ba6941
rykhlo <iurii.rykhlo@utexas.edu>
2021-05-31 11:05:25 -0500

    added reply and archive to single email display

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -46,6 +46,13 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   
   fetch(`emails/${mailbox}`)
   .then(response => response.json())
@@ -54,31 +61,82 @@ function load_mailbox(mailbox) {
         const card_div = document.createElement('div');
         card_div.setAttribute("class", `card`);
         card_div.setAttribute("id", `email-card`);
-        const header_div = document.createElement('div');
-        header_div.setAttribute("class", `card-header`)
-        const body_div = document.createElement('div');
-        body_div.setAttribute("class", `card-body`)
 
         card_div.innerHTML = `
           <div class="card-header">${email["sender"]}</div>
           <div class="card-body">
             <blockquote class="blockquote mb-0">
-              <p><small>Subject: </small>${email["subject"]}</p>
+              <p>${email["subject"]}</p>
               <footer class="blockquote-footer">${email["timestamp"]}</footer>
             </blockquote>
           </div>
         `;
 
         card_div.addEventListener('click', () => load_email(email));
-        document.querySelector('#emails-view').append(card_div);
-        
+        document.querySelector('#emails-view').append(card_div);   
       }
   });
+}
 
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
+function load_email(email) {
+  // Show the single email and hide other views
+  document.querySelector('#single-view').style.display = 'none';
+  document.querySelector('#single-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
-  // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  //set the read atribute of the email
+  fetch(`emails/${email["id"]}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+
+  //display the single email
+  const card_div = document.createElement('div');
+  card_div.setAttribute("class", `card`);
+  card_div.setAttribute("id", `email-card`);
+  card_div.innerHTML = `
+    <div>
+      <h6 class="card-header">From: ${email["sender"]}</h6>
+      <h6 class="card-header">To: ${email["recipients"]}</h6>
+      <h6 class="card-header">Subject: ${email["subject"]}</h6>
+      <h6 class="card-header">Time Sent: ${email["timestamp"]}</h6>
+      <div class="card-body">
+      <p class="card-text"> ${email["body"]}</p>
+    </div>
+    <hr>
+    <button type="button" class="btn btn-outline-secondary" id="reply">Reply</button>
+    <button type="button" class="btn btn-outline-secondary" id="archive">${email["archive"] ? "Remove from archived" : "Archive" }</button>
+  `;
+  document.querySelector('#single-view').innerHTML = ""; //clear the previous contents
+  document.querySelector('#single-view').append(card_div); 
+
+  //click on the archive button
+  document.querySelector('#archive').addEventListener('click', () => {
+    fetch(`emails/${email["id"]}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archive: !email["archived"],
+      })
+    })
+  });
+
+  //click on the reply button
+  document.querySelector('#reply').addEventListener('click', () => {
+    compose_email();
+    document.querySelector('#compose-recipients').value = email["sender"];
+    document.querySelector('#compose-body').value = `On ${email["timestamp"]} ${email["sender"]} wrote: \n ${email["body"]} \n \n`
+    let subject = email["subject"];
+    if (subject.slice(0,4) != "Re: "){
+      subject = `Re: ${subject}`;
+    }
+    document.querySelector('#compose-subject').value = subject; 
+  });
+
+  
+
+
+
 }
